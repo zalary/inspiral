474c27cc76c71cbff56a1d3248579892af3720ab
shukbot <adamshukur@gmail.com>
2014-03-30 18:10:33 -0500

    Generate user model, and add user/new view with form

---
+++ api/models/User.js
@@ -0,0 +1,37 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name: {
+      type: 'string',
+      required: true
+    },
+
+    username: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+    },
+
+    encryptedPassword: {
+      type: 'string'
+    }
+    
+  }
+
+};
