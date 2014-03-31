fac350f301956bb31755ab2c546433df141e1be4
Kathryn Beaty <kabeaty@gmail.com>
2014-03-31 15:47:36 -0500

    Add ability to create and destroy sessions

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -15,12 +15,74 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 
   'new': function(req, res) {
-    req.session.authenticated = true;
-    console.log(req.session);
+
     res.view('session/new');
+  },
+
+  create: function (req, res, next) {
+
+    //Check that email and password are entered in form.
+    if (!req.param('email') || !req.param('password')) {
+
+      //Return error if email and password aren't both entered in form.
+      var usernamePasswordRequiredError = [{name: 'usernamePasswordRequired', message: 'Enter both a username and password.'}]
+
+      //Store usernamePasswordRequired error in flash.err
+      req.session.flash = {
+        err: usernamePasswordRequiredError
+      }
+
+      res.redirect('/session/new');
+      return;
+    }
+
+    //Attempt to find user by email.
+    User.findOneByEmail(req.param('email'), function foundUser (err, user) {
+      if (err) return next(err);
+
+      if (!user) {
+        var noAccountError = [{name: 'noAccount', message: 'The email address ' + req.param('email') + ' is not found.'}]
+        req.session.flash = {
+          err: noAccountError
+        }
+        res.redirect('/session/new');
+        return;
+      }
+
+      // Compare form password params to the found user's encrypted password.
+      bcrypt.compare(req.param('password'), user.encryptedPassword, function(err, valid) {
+        if (err) return next(err);
+
+        //If password from form doesn't match the password from database
+        if (!valid) {
+          var usernamePasswordMismatchError = [{name: 'usernamePasswordMismatch', message: 'Invalid username and password combo.'}]
+          req.session.flash = {
+            err: usernamePasswordMismatchError
+          }
+          res.redirect('/session/new');
+          return;
+        }
+
+        req.session.authenticated = true;
+        req.session.User = user;
+
+        res.redirect('/user/show/' + user.id);
+      });
+    });
+  },
+
+  destroy: function(req, res, next) {
+
+    //Destroy session to log user out.
+    req.session.destroy();
+
+    //Redirect user to signin page.
+    res.redirect('/session/new');
   },
 
 
