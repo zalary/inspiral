2205ee481671d04878058937f326125e4a8661e0
shukbot <adamshukur@gmail.com>
2014-04-02 14:52:09 -0500

    Update policies to load passport policy before controllers

--- config/policies.js
+++ config/policies.js
@@ -16,7 +16,8 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access)
-  '*': 'flash'
+  '*': 'flash',
+  '*': [ 'passport' ]
 
   /*
 	// Here's an example of adding some policies to a controller
