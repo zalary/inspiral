6aca23cfde8865ceb75b58d4e53ffea2fdabce31
shukbot <adamshukur@gmail.com>
2014-04-02 15:56:12 -0500

    Add AuthController actions for login, process, and logout

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -15,16 +15,29 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+// Location: /api/controllers/AuthController.js
+var passport = require("passport");
 module.exports = {
-    
-  
+  login: function(req,res){
+    res.view("auth/login");
+  },
 
+  process: function(req,res){
+    passport.authenticate('local', function(err, user, info){
+      if ((err) || (!user)) {
+        res.redirect('/login');
+        return;
+      }
+      req.logIn(user, function(err){
+        if (err) res.redirect('/login');
+        return res.redirect('/');
+      });
+    })(req, res);
+  },
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to AuthController)
-   */
+  logout: function (req,res){
+    req.logout();
+    res.send('logout successful');
+  },
   _config: {}
-
-  
 };
