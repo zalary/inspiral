4c8f61dbc66219283a2108af1f8f41f102a0504b
Zalary Young <zalary@gmail.com>
2014-04-02 14:26:34 -0500

    .

--- assets/linker/js/inspire-custom.js
+++ assets/linker/js/inspire-custom.js
@@ -4,4 +4,23 @@
   // $('#nav').affix({
   //     offset: { top: $('#nav').offset().top() }
   // });
+
+  $('a.inspire_link').click(function(e){
+  e.preventDefault();
+  alert('hey ho');
+  var inspiration_id = 1;
+  var dataString = "inspiration_id=1&pinned_by=1&pinned_from=2";
+
+
+    $.ajax({
+          type: "POST",
+          url: "/story/create",
+          data: dataString,
+          success: function() {
+
+          }
+         });
+
+  });
+
 })();
