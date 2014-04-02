3154e147084c9d378abd8f9697c6537a2aeb4df5
Zalary Young <zalary@gmail.com>
2014-04-02 16:37:28 -0500

    inspiration list view split done and todo

--- assets/linker/js/backbone/views/inspiration-item-view.js
+++ assets/linker/js/backbone/views/inspiration-item-view.js
@@ -13,16 +13,4 @@ var itemTemplateHtml = $('#templates .inspiration-item').html(),
       }
     });
 
-    var doneItemTemplateHtml = $('#templates .done-inspiration-item').html(),
-    doneItemTemplate = _.template(doneItemTemplateHtml);
-
-    window.InspirationDoneView = Backbone.View.extend({
-      tagName: 'li',
-      className: 'list-group-item',
-      template: doneItemTemplate,
-      render: function() {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-      }
-    });
 })();
