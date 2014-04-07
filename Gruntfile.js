2f24d77bd59f5af7681146f8c0e665c4e88fc6c0
shukbot <adamshukur@gmail.com>
2014-04-07 12:04:32 -0500

    Add back the missing underscore.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,7 @@ module.exports = function (grunt) {
     // *->    put other dependencies here   <-*
     'linker/js/jquery-2.1.0.js',
     'linker/js/jquery.validate.min.js',
+    'linker/js/underscore-min.js',
     'linker/js/masonry.pkgd.min.js',
 
     // All of the rest of your app scripts imported here
