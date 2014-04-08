c4630ce5f555399241d44da879e17145eddc8835
shukbot <adamshukur@gmail.com>
2014-04-08 13:49:00 -0500

    Fix bug in app.js, improve backgrounds for inspirations

--- assets/linker/js/inspire-custom.js
+++ assets/linker/js/inspire-custom.js
@@ -1,10 +1,6 @@
 (function() {
   $('#nav-wrapper').height($("#nav").height());
 
-  // $('#nav').affix({
-  //     offset: { top: $('#nav').offset().top() }
-  // });
-
   $('a.inspire_link').click(function(e) {
     e.preventDefault();
     var inspiration_id = 1;
@@ -23,26 +19,22 @@
 $(document).ready(function() {
   // create an array of background papers
   var backgroundPapers = [];
-  backgroundPapers[0] = "/images/papers/farmer.png";
-  backgroundPapers[1] = "/images/papers/foil.png";
-  backgroundPapers[2] = "/images/papers/lined.png";
-  backgroundPapers[3] = "/images/papers/paper.png";
-  backgroundPapers[4] = "/images/papers/ricepaper2.png";
-  backgroundPapers[5] = "/images/papers/paper3.png";
-  backgroundPapers[6] = "/images/papers/textured.png";
-  backgroundPapers[7] = "/images/papers/paperboard.png";
+  backgroundPapers[0] = "/images/bgs/dust.png";
+  backgroundPapers[1] = "/images/bgs/food.png";
+  backgroundPapers[2] = "/images/bgs/pentagon.png";
+  backgroundPapers[3] = "/images/bgs/wood.png";
 
   // iterate through Dashboard and randomly assign width, height, background
   for (var i = 1; i <= $('div#notdone div.inspiration-item').length; i++) {
-    var width = Math.floor((Math.random() * 100)) + 75;
-    var height = Math.floor((Math.random() * 100)) + 75;
-    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * 8)];
+    var width = Math.floor((Math.random() * 75)) + 75;
+    var height = Math.floor((Math.random() * 75)) + 75;
+    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
     $('div#notdone div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
   };
   for (var i = 1; i <= $('div#done div.inspiration-item').length; i++) {
-    var width = Math.floor((Math.random() * 100)) + 75;
-    var height = Math.floor((Math.random() * 100)) + 75;
-    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * 8)];
+    var width = Math.floor((Math.random() * 75)) + 75;
+    var height = Math.floor((Math.random() * 75)) + 75;
+    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
 
     $('div#done div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
   }
@@ -50,9 +42,9 @@ $(document).ready(function() {
   // iterate through Feed and randomly assign width, height, background
 
   for (var i = 1; i <= $('div#inspiration-feed div.inspiration-item').length; i++) {
-    var width = Math.floor((Math.random() * 100)) + 75;
-    var height = Math.floor((Math.random() * 100)) + 75;
-    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * 8)];
+    var width = Math.floor((Math.random() * 75)) + 75;
+    var height = Math.floor((Math.random() * 75)) + 75;
+    var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
 
     $('div#inspiration-feed div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
   }
