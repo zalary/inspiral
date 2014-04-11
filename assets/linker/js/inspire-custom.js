5fa3c1a4ff6a872310bd576932deb6f0982d7f95
Zalary Young <zalary@gmail.com>
2014-04-11 08:21:14 -0500

    more style cleanup

--- assets/linker/js/inspire-custom.js
+++ assets/linker/js/inspire-custom.js
@@ -26,31 +26,31 @@
 
   // iterate through Dashboard and randomly assign width, height, background
   // for the todo dashboard column
-  (function() {
-    for (var i = 1; i <= $('#notdone div.inspiration-item').length; i++) {
-      var width = Math.floor((Math.random() * 75)) + 75;
-      //var height = Math.floor((Math.random() * 75)) + 75;
-      var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-      $('#notdone div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-    };
-  })();
-  // and the done dashboard column
-  (function() {
-    for (var i = 1; i <= $('#done div.inspiration-item').length; i++) {
-      var width = Math.floor((Math.random() * 75)) + 75;
-      //var height = Math.floor((Math.random() * 75)) + 75;
-      var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-      $('#done div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-    };
-  })();
-  // iterate through Feed and randomly assign width, height, background
-  (function() {
-    for (var i = 1; i <= $('#inspiration-feed div.inspiration-item').length; i++) {
-      //var width = Math.floor((Math.random() * 75)) + 125;
-      //var height = Math.floor((Math.random() * 75)) + 75;
-      var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-      $('#inspiration-feed div.inspiration-item:nth-child(' + i + ')').css("width", "190px").css("height", "100%").css("min-height", "150px");
-      //.css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-    };
-  })();
+  // (function() {
+  //   for (var i = 1; i <= $('#notdone div.inspiration-item').length; i++) {
+  //     var width = Math.floor((Math.random() * 75)) + 75;
+  //     //var height = Math.floor((Math.random() * 75)) + 75;
+  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
+  //     $('#notdone div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
+  //   };
+  // })();
+  // // and the done dashboard column
+  // (function() {
+  //   for (var i = 1; i <= $('#done div.inspiration-item').length; i++) {
+  //     var width = Math.floor((Math.random() * 75)) + 75;
+  //     //var height = Math.floor((Math.random() * 75)) + 75;
+  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
+  //     $('#done div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
+  //   };
+  // })();
+  // // iterate through Feed and randomly assign width, height, background
+  // (function() {
+  //   for (var i = 1; i <= $('#inspiration-feed div.inspiration-item').length; i++) {
+  //     //var width = Math.floor((Math.random() * 75)) + 125;
+  //     //var height = Math.floor((Math.random() * 75)) + 75;
+  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
+  //     $('#inspiration-feed div.inspiration-item:nth-child(' + i + ')').css("width", "190px").css("height", "100%").css("min-height", "150px");
+  //     //.css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
+  //   };
+  // })();
 })();
