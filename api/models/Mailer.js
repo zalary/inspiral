5047f02d3a7fc5d5c6de5f7cd55a713bbb59ef60
Kathryn Beaty <kabeaty@gmail.com>
2014-04-11 11:21:24 -0500

    Create Mailer controller and model for Nodemailer

---
+++ api/models/Mailer.js
@@ -0,0 +1,32 @@
+/**
+ * Mailer
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    email_to: {
+      type: 'string',
+      required: true
+    },
+
+    text: {
+      type: 'string',
+      required: true
+    },
+
+    subject: {
+      type: 'string'
+    }
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+
+  }
+
+};
