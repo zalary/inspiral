6f86d93fd421a9b94f2f96af4d30556f7e2dfc11
Zalary Young <zalary@gmail.com>
2014-04-15 00:35:58 -0500

    new homepage style

--- assets/linker/js/inspire-custom.js
+++ assets/linker/js/inspire-custom.js
@@ -1,3 +1,18 @@
+WebFontConfig = {
+  google: {
+    families: ['Lora:400,700,400italic,700italic:latin']['Montserrat:400,700:latin']['Lato:400,900:latin']
+  }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
 (function() {
   $('#nav-wrapper').height($("#nav").height());
 
@@ -14,59 +29,4 @@
       }
     });
   });
-})();
-
-(function() {
-  // create an array of background papers
-  var backgroundPapers = [];
-  backgroundPapers[0] = "/images/bgs/dust.png";
-  backgroundPapers[1] = "/images/bgs/food.png";
-  backgroundPapers[2] = "/images/bgs/pentagon.png";
-  backgroundPapers[3] = "/images/bgs/wood.png";
-
-  // iterate through Dashboard and randomly assign width, height, background
-  // for the todo dashboard column
-  // (function() {
-  //   for (var i = 1; i <= $('#notdone div.inspiration-item').length; i++) {
-  //     var width = Math.floor((Math.random() * 75)) + 75;
-  //     //var height = Math.floor((Math.random() * 75)) + 75;
-  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-  //     $('#notdone div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-  //   };
-  // })();
-  // // and the done dashboard column
-  // (function() {
-  //   for (var i = 1; i <= $('#done div.inspiration-item').length; i++) {
-  //     var width = Math.floor((Math.random() * 75)) + 75;
-  //     //var height = Math.floor((Math.random() * 75)) + 75;
-  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-  //     $('#done div.inspiration-item:nth-child(' + i + ')').css("width", width).css("height", height).css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-  //   };
-  // })();
-  // // iterate through Feed and randomly assign width, height, background
-  // (function() {
-  //   for (var i = 1; i <= $('#inspiration-feed div.inspiration-item').length; i++) {
-  //     //var width = Math.floor((Math.random() * 75)) + 125;
-  //     //var height = Math.floor((Math.random() * 75)) + 75;
-  //     var backgroundPaperUrl = backgroundPapers[Math.floor(Math.random() * backgroundPapers.length)];
-  //     $('#inspiration-feed div.inspiration-item:nth-child(' + i + ')').css("width", "190px").css("height", "100%").css("min-height", "150px");
-  //     //.css("background-image", 'url("' + backgroundPaperUrl + '")').css("opacity", .85);
-  //   };
-  // })();
-})();
-
-
-WebFontConfig = {
-  google: {
-    families: ['Lato:400,900:latin']
-  }
-};
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
 })();
