af77e831c8f60b80a15080bffb261afb1eee760e
Adam Shukur <shukbot@users.noreply.github.com>
2014-04-15 17:05:59 -0500

    Update adapters.js
    Commented out for local development

--- config/adapters.js
+++ config/adapters.js
@@ -39,28 +39,28 @@ module.exports.adapters = {
 
   //DEVELOPMENT ONLY
 
-  'default': 'psdb',
+  // 'default': 'psdb',
 
-  'psdb': {
-    module: 'sails-postgresql',
-    host: 'ec2-23-23-81-171.compute-1.amazonaws.com' || 'localhost',
-    user: process.env.HK_POSTGRES_USER || 'zalary',
-    password: process.env.HK_POSTGRES_PASS || '',
-    database: 'd7h97cqpalnj0q' || 'inspiral',
-    port: 5432,
-    //this should be set to false when working locally,
-    //but must be set to true for heroku deployment
-    ssl: true,
+  // 'psdb': {
+  //   module: 'sails-postgresql',
+  //   host: 'ec2-23-23-81-171.compute-1.amazonaws.com' || 'localhost',
+  //   user: process.env.HK_POSTGRES_USER || 'zalary',
+  //   password: process.env.HK_POSTGRES_PASS || '',
+  //   database: 'd7h97cqpalnj0q' || 'inspiral',
+  //   port: 5432,
+  //   //this should be set to false when working locally,
+  //   //but must be set to true for heroku deployment
+  //   ssl: true,
 
-    schema: true //This makes sure that sails matches
-    //the database schema to your models.
-  },
+  //   schema: true //This makes sure that sails matches
+  //   //the database schema to your models.
+  // },
 
 
-  'mongo': {
+  // 'mongo': {
 
-    module: 'sails-mongo',
-    url: process.env.MHQ_URL || ''
+  //   module: 'sails-mongo',
+  //   url: process.env.MHQ_URL || ''
 
-  }
+  // }
 };
