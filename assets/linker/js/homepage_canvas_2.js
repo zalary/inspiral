8258254ba0f446ae7ae7159820fd0cccba75ef90
Kathryn Beaty <kabeaty@gmail.com>
2014-04-14 21:17:12 -0500

    Update positioning of Paper canvas on homepage and adjust x/y values

--- assets/linker/js/homepage_canvas_2.js
+++ assets/linker/js/homepage_canvas_2.js
@@ -3,9 +3,9 @@ var leftPath, rightPath, newCircles, randomNum;
 //Set up colors array to randomize circles
 var colors = ['#143B7F', '#74A6FF', '#2876FF', '#3A537F', '#205ECC'];
 
-var x = 0;
+var x = 50;
 var y = 250;
-var i = 720;
+var i = 850;
 var j = 250;
 var radius = 10;
 
@@ -15,7 +15,7 @@ function onFrame(event) {
   if (event.count % 5 == 0) {
 
     //The first time, instantiate new paths
-    if (x == 0) {
+    if (x == 50) {
       leftPath = new Path();
       rightPath = new Path();
       leftPath.strokeColor = '#FF6600';
@@ -27,7 +27,7 @@ function onFrame(event) {
     //Logic for the line from the left
     leftPath.add(new Point(x, y));
     leftPath.smooth();
-    if (x < 360) {
+    if (x < 450) {
       x += 10;
       if (y === 250 || y === 220) {
         y += 30;
@@ -39,7 +39,7 @@ function onFrame(event) {
     //Logic for the line from the right
     rightPath.add(new Point(i, j));
     rightPath.smooth();
-    if (i > 360) {
+    if (i > 450) {
       i -= 10;
       if (j === 250 || j == 220) {
         j += 30;
@@ -49,13 +49,13 @@ function onFrame(event) {
     }
 
     //Logic for circles spiralling out when paths touch in middle
-    if (x == 360) {
-      var explodingCircle = new Path.Circle(new Point(360, 250), 10);
+    if (x == 450) {
+      var explodingCircle = new Path.Circle(new Point(450, 250), 10);
       explodingCircle.fillColor = '#428bca';
       explodingCircle.sendToBack();
 
      if (radius <= 390) {
-      newCircles = new Path.Circle(new Point(360, 250), radius);
+      newCircles = new Path.Circle(new Point(450, 250), radius);
       randomNum = Math.round(Math.random() * 5);
       newCircles.fillColor = colors[randomNum];
       newCircles.sendToBack();
@@ -64,9 +64,9 @@ function onFrame(event) {
       //Clear project layers and reset variables to start over
       if (radius == 400) {
         project.activeLayer.removeChildren();
-        x = 0;
+        x = 50;
         y = 250;
-        i = 720;
+        i = 850;
         j = 250;
         radius = 10;
       }
